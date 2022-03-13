0ed32df1cedeb860780ab175ae50c75796dbeef7
Silvio Pettauer <sipettauer@edu.aau.at>
2022-03-13 17:32:35 +0100

    Client Server Interaction finished but App is closing when Button is pushed

--- app/src/main/java/com/example/einzelbeispiel_pettauer_silvio/sendtoserver.java
+++ app/src/main/java/com/example/einzelbeispiel_pettauer_silvio/sendtoserver.java
@@ -1,64 +1,52 @@
 package com.example.einzelbeispiel_pettauer_silvio;
 
-import android.os.AsyncTask;
-
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 public class sendtoserver extends Thread {
 
-        Socket s;
-        DataOutputStream dos;
-        PrintWriter pw;
-        BufferedReader brd;
-        BufferedReader brdanswer;
-        InputStream str;
-        String answer;
+    String answer;
+    InputStream str;
 
     public sendtoserver(InputStream str) {
         this.str = str;
     }
 
-    protected String sendmsg(InputStream msg) {
-
-        InputStream message = msg;
-
-        try {
-
-            s = new Socket("se2-isys.aau.at",53212);
-            brd = new BufferedReader(new InputStreamReader(message));
-            brdanswer = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            dos =  new DataOutputStream(s.getOutputStream());
-            pw = new PrintWriter(s.getOutputStream());
-
-            String servermsg = brd.readLine();
-            dos.writeBytes(servermsg);
-            String answer = brdanswer.readLine();
-            s.close();
-            return answer;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
     @Override
     public void run() {
         try {
-            answer = sendmsg(str);
+            answer = getanswer(str);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public String getanswer() {
+    public String getanswer(InputStream stream) throws Exception{
+        String sentence;
+        String modifiedsentence;
+
+        BufferedReader brd = new BufferedReader(new InputStreamReader(stream));
+
+        Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        sentence = brd.readLine();
+
+        outToServer.writeBytes((sentence + '\n'));
+
+        modifiedsentence = inFromServer.readLine();
+
+        clientSocket.close();
+        return modifiedsentence;
+
+
+    }
+
+    public String getAnswer() {
         return answer;
     }
 }
